f60c14774ae5bedead0625c55711132676aa605b
thembu <rakhivhanithembu@gmail.com>
2024-04-30 14:34:07 +0200

    added the background image

--- app/src/main/java/com/example/pharmarcyapp/MainActivity.java
+++ app/src/main/java/com/example/pharmarcyapp/MainActivity.java
@@ -1,8 +1,10 @@
 package com.example.pharmarcyapp;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
 
 import android.os.Bundle;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -11,4 +13,6 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
     }
+
+
 }
